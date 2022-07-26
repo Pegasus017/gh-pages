16089d28f284b12d4feede9c0965ab1c797fb26b
Pegasus017 <makh007@naver.com>
2022-07-26 18:12:40 +0900

    4th js challenge done
    the d-day counter

---
+++ 4th js/src/index.js
@@ -0,0 +1,20 @@
+const clockTitle = document.querySelector(".js-clock");
+
+function getClock(){
+    let today = new Date();
+    let dDay = new Date(2022,11,25);
+    let gap = dDay.getTime() - today.getTime();
+    let resultSeconds = Math.floor((gap%(1000*60))/1000);
+    let resultMinutes = Math.floor((gap%(1000*60*60))/(1000*60));
+    let resultHours = Math.floor((gap%(1000*60*60*24))/(1000*60*60));
+    let resultDays = Math.floor(gap/(1000*60*60*24));
+
+
+    let seconds = String(resultSeconds).padStart(2,"0");
+    
+    clockTitle.innerText=`Until Christmas, ${resultDays}d ${resultHours}h ${resultMinutes}m ${seconds}s left.`;
+    
+}
+
+getClock();
+setInterval(getClock, 1000);
