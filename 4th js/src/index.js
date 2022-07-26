9fe61483c797c4c4c55b16baf21eeb3f7e8df5ea
Pegasus017 <makh007@naver.com>
2022-07-26 18:26:57 +0900

    changed date function
    not giving minus 1 to the month, changed the format of the date to the exact one

--- 4th js/src/index.js
+++ 4th js/src/index.js
@@ -1,19 +1,19 @@
 const clockTitle = document.querySelector(".js-clock");
 
-function getClock(){
+function getClock() {
     let today = new Date();
-    let dDay = new Date(2022,11,25);
+    let dDay = new Date("december 25, 2022 0:00:00");
     let gap = dDay.getTime() - today.getTime();
-    let resultSeconds = Math.floor((gap%(1000*60))/1000);
-    let resultMinutes = Math.floor((gap%(1000*60*60))/(1000*60));
-    let resultHours = Math.floor((gap%(1000*60*60*24))/(1000*60*60));
-    let resultDays = Math.floor(gap/(1000*60*60*24));
+    let resultSeconds = Math.floor((gap % (1000 * 60)) / 1000);
+    let resultMinutes = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
+    let resultHours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let resultDays = Math.floor(gap / (1000 * 60 * 60 * 24));
 
 
-    let seconds = String(resultSeconds).padStart(2,"0");
-    
-    clockTitle.innerText=`Until Christmas, ${resultDays}d ${resultHours}h ${resultMinutes}m ${seconds}s left.`;
-    
+    let seconds = String(resultSeconds).padStart(2, "0");
+
+    clockTitle.innerText = `Until Christmas, ${resultDays}d ${resultHours}h ${resultMinutes}m ${seconds}s left.`;
+
 }
 
 getClock();
