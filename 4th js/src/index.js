3f0681540b7308f58bf516def3e3ca4e6fb9f81c
Pegasus017 <makh007@naver.com>
2022-07-27 09:24:57 +0900

    4th js challenge
    the background linear-gradient color change from the array by clicking the button

---
+++ 4th js/src/index.js
@@ -0,0 +1,36 @@
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+  ];
+
+  const button = document.querySelector("button");
+  const background = document.querySelector("body");
+
+function changeColor() {
+    let color1 = colors[Math.round(Math.random()*10)];
+    let color2 = colors[Math.round(Math.random()*10)];
+
+    if(color1 === color2){
+        color2 = colors[Math.round(Math.random()*10)];
+    }else{
+        background.style.background = `linear-gradient(0.25turn, ${color1}, ${color2})`;
+    }
+}
+
+addEventListener("click",changeColor);
