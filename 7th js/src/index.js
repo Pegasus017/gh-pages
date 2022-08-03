1c38f3368e8bb71ad77e91a345a99fdd95b44c57
Pegasus017 <makh007@naver.com>
2022-08-04 01:29:44 +0900

    done 80% of the challenge
    123

--- 7th js/src/index.js
+++ 7th js/src/index.js
@@ -9,3 +9,34 @@ canvas.height = 600;
 ctx.lineWidth = 5;
 let isPainting = false;
 let isFilling = false;
+
+function startPainting(event){
+    isPainting = true;
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function onMove(event){
+    if (isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+}
+
+function cancelPainting(event){
+    isPainting = false;
+    ctx.beginPath();
+}
+
+function onColorChange(event){
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+    color.value = event.target.value;
+}
+
+canvas.addEventListener("mousedown",startPainting);
+canvas.addEventListener("mousemove",onMove);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+
+color.addEventListener("change", onColorChange);
