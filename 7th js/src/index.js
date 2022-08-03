36936c6b24148e2768e1e7c1be4aada6f41fc9c6
Pegasus017 <makh007@naver.com>
2022-08-03 18:48:12 +0900

    7th js
    done the basic setup

---
+++ 7th js/src/index.js
@@ -0,0 +1,11 @@
+const canvas = document.querySelector("canvas");
+const color = document.getElementById("color");
+const fill = document.getElementById("fill");
+const stroke = document.getElementById("stroke");
+const ctx = canvas.getContext("2d");
+canvas.width = 600;
+canvas.height = 600;
+
+ctx.lineWidth = 5;
+let isPainting = false;
+let isFilling = false;
